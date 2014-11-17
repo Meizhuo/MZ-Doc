b9366a4c459204bb10262c8a15042aaa580d5e92
Jayin Ton <tonjayin@gmail.com>
2014-11-17 20:41:30 +0800

    ~
    Signed-off-by: Jayin Ton <tonjayin@gmail.com>

--- 数据备份.bat
+++ 数据备份.bat
@@ -3,7 +3,7 @@ SET TM= mz%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.sql
 
 echo 开始数据备份
 
-E:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump mz > %TM%
+D:\web\wamp\bin\mysql\mysql5.6.17\bin\mysqldump mz > %TM%
 
 echo 数据备份-%TM% 完成
 @echo on
